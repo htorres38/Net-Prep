e3cccd918b17fa6ce2525fa8e6ee5ab327823a13
htorres38 <hjade7255@gmail.com>
2026-03-22 19:36:15 -0500

    Fix: use WASQLiteOpenFactory for flags config

--- src/lib/powersync/db.ts
+++ src/lib/powersync/db.ts
@@ -4,17 +4,17 @@ import { SupabaseConnector } from './connector'
 export const connector = new SupabaseConnector()
 
 export async function createPowerSyncDb() {
-  const { PowerSyncDatabase } = await import('@powersync/web')
+  const { PowerSyncDatabase, WASQLiteOpenFactory } = await import('@powersync/web')
   const db = new PowerSyncDatabase({
     schema: AppSchema,
-    database: {
+    database: new WASQLiteOpenFactory({
       dbFilename: 'prepnet.db',
       flags: {
         disableSSRWarning: true,
         useWebWorker: false,
         enableMultiTabs: false,
       },
-    },
+    }),
   })
   await db.init()
   await db.connect(connector)
